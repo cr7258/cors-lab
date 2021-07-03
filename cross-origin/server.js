875d9fd0f9ade456bad7b44abdaf2e2c8b76f501
chengzw <chengzw258@163.com>
2021-07-03 18:16:08 +0800

    跨域问题总结

---
+++ cross-origin/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const PORT = 3011;
+
+http.createServer((req, res) => {
+  const url = req.url;
+  console.log('request url: ', url);
+  if (url === '/api/data') {
+    return res.end('console.log("hello world!");');
+  }
+}).listen(PORT);
+
+console.log('Server listening on port ', PORT);
